69723cfbbe12a5d28abb2005efecf47d2eda0f6c
Grigoriy <grisha_qa@mail.ru>
2025-05-05 17:57:17 +0300

    a

--- test_demo.py
+++ test_demo.py
@@ -1,8 +1,18 @@
-new*
-def demo1():
-    assert 1 == 1
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
 
-new*
-def demo2():
-    assert 1 == 2
 
+@pytest.fixture()
+def browser():
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.implicitly_wait(10)
+    yield browser
+
+def test_open_Forgote_password(browser):
+    browser.get('https://test.hu-li.com/en/auth/login')
+    forgot_password = browser.find_element(By.LINK_TEXT, 'Forgot password?')
+    forgot_password.click()
+    title = browser.find_element(By.CSS_SELECTOR, 'h1')
+    assert title.text == 'Forgot password?'
